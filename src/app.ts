aa0c13d99dddd516b9c3739710498a571b6a2c0e
abenjamin1313 <abenjamin1313@gmail.com>
2018-05-06 13:32:33 -0400

    Finished Contact manager app.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
-    import {PLATFORM} from 'aurelia-pal';
+    import {inject, PLATFORM} from 'aurelia-framework';
+    import {WebAPI} from './web-api';
 
+    @inject(WebAPI)
     export class App {
       router: Router;
 
-      configureRouter(config: RouterConfiguration, router: Router){
+      constructor(public api: WebAPI) {}
+
+      configureRouter(config: RouterConfiguration, router: Router) {
         config.title = 'Contacts';
         config.map([
-          { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select' },
+          { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select'},
           { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
         ]);
 
